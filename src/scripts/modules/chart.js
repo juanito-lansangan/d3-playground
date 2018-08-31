cfa9792f235e77f4b16add76276d5d7a9f21bf44
Juanito Lansangan <jlansangan@ko-kard.com>
2018-08-31 17:14:37 +0800

    use svg for bar chart

--- src/scripts/modules/chart.js
+++ src/scripts/modules/chart.js
@@ -13,20 +13,40 @@ export default class Chart {
 
   drawBarChart() {
     const data = [4, 8, 15, 16, 23, 42];
+    const width = 420;
+    const barHeight = 20;
 
     const x = d3
       .scaleLinear()
       .domain([0, d3.max(data)])
-      .range([0, 420]);
+      .range([0, width]);
 
-    d3.select(".chart")
-      .selectAll("div")
+    const chart = d3
+      .select(".chart")
+      .attr("width", width)
+      .attr("height", barHeight * data.length);
+
+    const bar = chart
+      .selectAll("g")
       .data(data)
       .enter()
-      .append("div")
-      .style("width", function(d) {
-        return x(d) + "px";
+      .append("g")
+      .attr("transform", function(d, i) {
+        return `translate(0, ${i * barHeight})`;
+      });
+
+    bar
+      .append("rect")
+      .attr("width", x)
+      .attr("height", barHeight - 1);
+
+    bar
+      .append("text")
+      .attr("x", function(d) {
+        return x(d) - 3;
       })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
       .text(function(d) {
         return d;
       });
