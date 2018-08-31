a59d88484ae23b22d641435ff4aca201d615e13e
Juanito Lansangan <jlansangan@ko-kard.com>
2018-08-31 16:54:03 +0800

    initial commit

---
+++ src/scripts/modules/chart.js
@@ -0,0 +1,34 @@
+import * as d3 from "d3";
+
+export default class Chart {
+  constructor(el) {
+    console.log(el);
+    this.init();
+  }
+
+  init() {
+    console.log("initializing Chart script...");
+    this.drawBarChart();
+  }
+
+  drawBarChart() {
+    const data = [4, 8, 15, 16, 23, 42];
+
+    const x = d3
+      .scaleLinear()
+      .domain([0, d3.max(data)])
+      .range([0, 420]);
+
+    d3.select(".chart")
+      .selectAll("div")
+      .data(data)
+      .enter()
+      .append("div")
+      .style("width", function(d) {
+        return x(d) + "px";
+      })
+      .text(function(d) {
+        return d;
+      });
+  }
+}
