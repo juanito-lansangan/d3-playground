4be2d40d2eeba4a24f38352dad12592f8c914e7b
jplans <juanito.lansangan@gmail.com>
2018-09-01 14:25:20 +0800

    rotate bar chart vertically, add axes for x and y of the chart

--- src/scripts/modules/chart.js
+++ src/scripts/modules/chart.js
@@ -12,53 +12,80 @@ export default class Chart {
   }
 
   drawBarChart() {
-    const data = [4, 8, 15, 16, 23, 42];
-    const width = 420;
-    const barHeight = 20;
+    // const width = 420;
+    // const barHeight = 20;
 
-    const x = d3.scaleLinear().range([0, width]);
+    // const x = d3.scaleLinear().range([0, width]);
 
-    const chart = d3.select(".chart").attr("width", width);
+    // const chart = d3.select(".chart").attr("width", width);
     // .attr("height", barHeight * data.length);
 
-    d3.tsv("/data/data.tsv", this.type).then(data => {
+    // d3.tsv("/data/data.tsv", this.type).then(data => {
+    //   console.log(data);
+    // });
+    const svg = d3.select("svg"),
+      margin = { top: 20, right: 20, bottom: 30, left: 40 },
+      width = +svg.attr("width") - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom;
+
+    const x = d3
+        .scaleBand()
+        .rangeRound([0, width])
+        .padding(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]);
+
+    const g = svg
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.tsv("/data/data.tsv", function(d) {
+      d.frequency = +d.frequency;
+      return d;
+    }).then((data, error) => {
       console.log(data);
-      x.domain([
+      if (error) throw error;
+
+      x.domain(
+        data.map(function(d) {
+          return d.letter;
+        })
+      );
+      y.domain([
         0,
         d3.max(data, function(d) {
-          return d.value;
+          return d.frequency;
         })
       ]);
 
-      const bar = chart
-        .selectAll("g")
+      g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10, "%"))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Frequency");
+
+      g.selectAll(".bar")
         .data(data)
         .enter()
-        .append("g")
-        .attr("transform", function(d, i) {
-          console.log(d, i);
-          return `translate(0, ${i * barHeight})`;
-        });
-      // console.log(x);
-      bar
         .append("rect")
-        .attr("width", function(d) {
-          return x(d.value);
-        })
-        .attr("height", barHeight - 1);
-
-      bar
-        .append("text")
+        .attr("class", "bar")
         .attr("x", function(d) {
-          console.log("====================================");
-          console.log(d);
-          console.log("====================================");
-          return x(d.value) - 3;
+          return x(d.letter);
         })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) {
-          return d.value;
+        .attr("y", function(d) {
+          return y(d.frequency);
+        })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) {
+          return height - y(d.frequency);
         });
     });
   }
